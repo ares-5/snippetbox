049148bac40988930c85e575622bc6047c67f004
Zafirovic <lazar.zafirovic@elfak.rs>
2024-02-12 22:30:26 +0100

    - Add User registration - Add User login - Add User logout - Add Authorization middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -25,6 +25,21 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 	})
 }
 
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		// Otherwise set the "Cache-Control: no-store" header so that pages
+		// require authentication are not stored in the users browser cache (or
+		// other intermediary cache).
+		w.Header().Add("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
